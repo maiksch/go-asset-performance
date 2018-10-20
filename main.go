9affe783be04d2dd7ad95bc3868c99b64525107c
Maik Schmaddebeck <maik.schmaddebeck@point-gmbh.com>
2018-10-20 21:26:15 +0200

    Removed not used main.go file

--- main.go
+++
@@ -1,8 +0,0 @@
-package performance
-
-import "fmt"
-
-// Get asks the yahoo finance API for the performance of the given symbols
-func Get(symbols ...[]string) {
-	fmt.Println("Getting performancce of symbols", symbols)
-}
